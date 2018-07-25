3289c6a8553f6f2d679fe3c3d852d76bf5820fea
Gerry <gehard@midlight.gmbh>
2018-07-25 03:13:50 +0200

    swiper

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
 //import {PLATFORM} from 'aurelia-pal';
-
+import Swiper from 'swiper';
 export class App {
   configureRouter(config, router) {
     config.title = 'Aurelia';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: PLATFORM.moduleName('./welcome'),      nav: true, title: 'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: PLATFORM.moduleName('./users'),        nav: true, title: 'Github Users' },
-      { route: 'child-router',  name: 'child-router', moduleId: PLATFORM.moduleName('./child-router'), nav: true, title: 'Child Router' }
+      { route: ['', 'welcome'], name: 'welcome',      moduleId: PLATFORM.moduleName('./welcome'),      nav: false, title: 'Welcome' },
+      { route: 'users',         name: 'users',        moduleId: PLATFORM.moduleName('./users'),        nav: false, title: 'Github Users' },
+      { route: 'child-router',  name: 'child-router', moduleId: PLATFORM.moduleName('./child-router'), nav: false, title: 'Child Router' }
     ]);
 
     this.router = router;
+  }
+
+  startSwiper(){
+    let menuSwiper = new Swiper('#swiper', {
+    })
+  }
+
+  attached(){
+    this.startSwiper();
   }
 }
