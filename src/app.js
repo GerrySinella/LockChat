d64063fdf7f7e1f6148e34c45ea5b09f201b4c8f
Gerry <gehard@midlight.gmbh>
2018-07-26 20:06:14 +0200

    swiper

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Aurelia';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: PLATFORM.moduleName('./welcome'),      nav: false, title: 'Welcome' },
+      { route: ['', 'welcome'], name: 'welcome',      moduleId: PLATFORM.moduleName('./welcome'),      nav: false, title: 'Welcome', setting: {
+        active: true
+          }},
       { route: 'users',         name: 'users',        moduleId: PLATFORM.moduleName('./users'),        nav: false, title: 'Github Users' },
       { route: 'child-router',  name: 'child-router', moduleId: PLATFORM.moduleName('./child-router'), nav: false, title: 'Child Router' }
     ]);
