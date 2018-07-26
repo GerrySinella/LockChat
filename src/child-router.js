037a63f5c7530eaf471e159a097012295c0df3da
Valunex <Amon1998>
2018-07-26 23:40:42 +0200

    navigation bar design änderung

--- src/child-router.js
+++ src/child-router.js
@@ -5,9 +5,10 @@ export class ChildRouter {
 
   configureRouter(config, router) {
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: PLATFORM.moduleName('./welcome'),      nav: true, title: 'Welcome' },
+      { route: ['chats'], name: 'chats',      moduleId: PLATFORM.moduleName('./chats'),      nav: true, title: 'chats' },
       { route: 'users',         name: 'users',        moduleId: PLATFORM.moduleName('./users'),        nav: true, title: 'Github Users' },
-      { route: 'child-router',  name: 'child-router', moduleId: PLATFORM.moduleName('./child-router'), nav: true, title: 'Child Router' }
+      { route: 'child-router',  name: 'child-router', moduleId: PLATFORM.moduleName('./child-router'), nav: true, title: 'Child Router' },
+        { route: ['', 'willkommen'],  name: 'willkommen', moduleId: PLATFORM.moduleName('./willkommen'), nav: true, title: 'willkommen' }
     ]);
 
     this.router = router;
