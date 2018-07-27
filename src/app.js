07f1d34f745f0477dac36bf09b73899af5d832f7
Gerry <gehard@midlight.gmbh>
2018-07-27 09:14:49 +0200

    navbar swiper navigation

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 //import {PLATFORM} from 'aurelia-pal';
-import Swiper from 'swiper' ;
+import {autoinject} from 'aurelia-framework';
+import Swiper from 'swiper';
 
+autoinject;
 export class App {
     menuswiper = this.menuswiper;
     menuswiperNav = this.menuswiperNav;
-
     configureRouter(config, router) {
         config.title = 'Aurelia';
         config.map([
@@ -40,14 +41,53 @@ export class App {
                 el: $('#navabr'),
                 clickable: true,
                 renderBullet: function (index, classname) {
-                    return '<div class="col-4 navbaricon ' + classname + '"><a><inline-svg class="svg icons" svg="svg/00' + (index + 1) + '.svg"></inline-svg></a></div>';
+                    return '<div class="col-4 navbaricon ' + classname + '"><a><img class="svg icons" src="svg/00' + (index + 1) + '.svg"></a></div>';
                 },
             },
         });
     }
 
+    changeSvgColor(){
+        $('img.svg').each((i, e) => {
+
+            const $img = $(e);
+
+        const imgID = $img.attr('id');
+
+        const imgClass = $img.attr('class');
+
+        const imgURL = $img.attr('src');
+
+        $.get(imgURL, (data) => {
+            // Get the SVG tag, ignore the rest
+            let $svg = $(data).find('svg');
+
+        // Add replaced image's ID to the new SVG
+        if (typeof imgID !== 'undefined') {
+            $svg = $svg.attr('id', imgID);
+        }
+        // Add replaced image's classes to the new SVG
+        if (typeof imgClass !== 'undefined') {
+            $svg = $svg.attr('class', `${imgClass}replaced-svg`);
+        }
+
+        // Remove any invalid XML tags as per http://validator.w3.org
+        $svg = $svg.removeAttr('xmlns:a');
+
+        // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+        if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+            $svg.attr(`viewBox 0 0  ${$svg.attr('height')} ${$svg.attr('width')}`);
+        }
+
+        // Replace image with new SVG
+        $img.replaceWith($svg);
+    }, 'xml');
+    });
+    }
+
 
     attached() {
         this.startSwiper();
+        this.changeSvgColor();
     }
 }
